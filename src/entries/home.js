8ad54ef6636ae939ec2df9355399aa918c3a9c06
Joel Yustiz <yustizjoel17@gmail.com>
2018-01-14 10:13:24 -0400

    Video 22 estructura del proyecto

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 // import Playlist from './src/playlist/components/playlist'
-// import data from '../api.json'
+import Data from '../api.json'
 import Home from '../pages/containers/home'
 
 const app = document.getElementById('app')
 
 
-render( < Home /> , app);
+render( < Home data={Data}/> , app);
--- src/pages/components/home-layout.js
+++ src/pages/components/home-layout.js
@@ -3,7 +3,6 @@ import React from 'react'
 function HomeLayout(props) {
     return(
         <section>
-            layou
             {props.children}
         </section>
     )
